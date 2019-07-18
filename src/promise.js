54e44bfef478e72305692664f13533b550db8f78
vigdorov <vignikbor@gmail.com>
2019-07-18 21:51:37 +0300

    complete promise

--- src/promise.js
+++ src/promise.js
@@ -1,40 +1,33 @@
 const app = document.getElementById('app');
 
-// Создали промис, внутри которого происходит обращение к серверу
-let promise = fetch('https://api.tvmaze.com/search/shows?q=batman');
+const inputName = document.getElementById('input-find');
+const findBtn = document.getElementById('find');
+findBtn.addEventListener('click', function() {
+  find(inputName.value);
+});
 
+function find(name) {
+  let promise = fetch(`https://api.tvmaze.com/search/shows?q=${name}`);
 
-promise
-  .then( function (response) {
-    return response.json();
-  })
-  .then( function (data) {
-    console.log(data);
-    render(data);
-  });
-let btnClick = 0;
-function show(index) {
-    let cardImg = document.getElementsByClassName ('card__img');
-    let descr = document.getElementsByClassName ('description');
-    let cardBtn = document.getElementsByClassName ('read-more');
-    if (btnClick === 0){
-        descr[index].style.height = 'auto';
-        cardImg[index].style.display = 'none';
-        cardBtn[index].textContent = "Скрыть описание";
-        return btnClick = 1;
-    } else if (btnClick === 1){
-        descr[index].style.height = '110px';
-        cardImg[index].style.display = 'block';
-        cardBtn[index].textContent = "Подробное описание";
-        return btnClick = 0;
-    }
-
+  promise
+    .then( function (response) {
+      return response.json();
+    })
+    .then( function (data) {
+      console.log(data);
+      render(data);
+    });
 }
+// Создали промис, внутри которого происходит обращение к серверу
+let btnStateClick = [];
 
 function render (data) {
+  btnStateClick = [];
+
   const cards = [];
 
   data.forEach( function (film, index)  {
+    btnStateClick.push(0);
     cards.push(
       `
       <div class="card">
@@ -52,3 +45,19 @@ function render (data) {
   app.innerHTML = cards.join('');
 }
 
+function show(index) {
+  let cardImg = document.getElementsByClassName ('card__img');
+  let descr = document.getElementsByClassName ('description');
+  let cardBtn = document.getElementsByClassName ('read-more');
+  if (btnStateClick[index] === 0){
+    descr[index].style.height = 'auto';
+    cardImg[index].style.display = 'none';
+    cardBtn[index].textContent = "Скрыть описание";
+    btnStateClick[index] = 1;
+  } else if (btnStateClick[index] === 1){
+    descr[index].style.height = '110px';
+    cardImg[index].style.display = 'block';
+    cardBtn[index].textContent = "Подробное описание";
+    btnStateClick[index] = 0;
+  }
+}
